95047fa4767786ed0425adfea4e0e0248133a8bc
GitHub <noreply@github.com>
2025-08-28 12:59:27 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Book",
   "number": "",
   "title": "GIRAFFE",
-  "body": " GIRAFFE  Graph Inference and Relational Analysis for Frameworks & Feature Extraction      About GIRAFFE   The GIRAFFE Workshop is a week-long, collaborative event held at WSSU with the goal of fostering original research with undergraduate students in the mathematical sciences. The program is focused on mentorship, hands-on learning, and the development of research skills in the mathematical sciences. A particular emphasis is placed on combinatorics and data science for their real-world applications and low start-up cost for students with a wide range of mathematical maturity. The workshop employs a model proven effective in a variety of settings supported by the mathematical science institutes, including the American Institute of Mathematics and SLMath.     Travel  Text of section.    Typical Daily Schedule      "
+  "body": " GIRAFFE  Graph Inference and Relational Analysis for Frameworks & Feature Extraction      About GIRAFFE   The GIRAFFE Workshop is a week-long, collaborative event held at WSSU with the goal of fostering original research with undergraduate students in the mathematical sciences. The program is focused on mentorship, hands-on learning, and the development of research skills in the mathematical sciences. A particular emphasis is placed on combinatorics and data science for their real-world applications and low start-up cost for students with a wide range of mathematical maturity. The workshop employs a model proven effective in a variety of settings supported by the mathematical science institutes, including the American Institute of Mathematics and SLMath.      Travel  Text of section.    Typical Daily Schedule      "
 }
 ]
 
